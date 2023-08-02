b71458cee5c46aef2301ce44344ec7b714ef3177
Marcelo Machado <mjoelmachadojr@gmail.com>
2023-08-02 20:34:05 -0300

    started with C

---
+++ scripts/script.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main() {
+
+    char target[] = "google.com";
+    int door = 80;
+
+    printf("Start sending packages to target %s, using door %i", target, door);
+
+    return 0;
+}
