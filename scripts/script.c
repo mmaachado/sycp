7d64087b921cbbba61817959c54590340b6450a4
mmaachado <mjoelmachadojr@gmail.com>
2023-08-03 23:23:01 -0300

    finished aula 3

--- scripts/script.c
+++ scripts/script.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int main() {
 
     char target[100]; 
+    char response[3];
     int door;
 
     printf("Type the target:\n"); 
@@ -12,7 +13,14 @@ int main() {
     printf("Type the door:\n"); 
     scanf("%i", &door);
 
-    printf("Starting attack on target %s, in door %i", target, door);
+    printf("Do you confirm the attack on: %s\n", target);
+    scanf("%s", response);
+
+    if (strcmp(response, "y") == 0) {
+        printf("Starting attack on target %s, using door %i", target, door);
+    } else {
+        printf("Stopping attack. . .");
+    }
 
     return 0;
 }
